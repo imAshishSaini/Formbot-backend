289e151a5255765892cb0a35316f0114a93ec0af
imAshishSaini <19bcs1363@gmail.com>
2025-01-04 00:47:07 +0530

    update

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ dotenv.config()
 const app = express()
 const cors = require('cors')
 
-app.use(cors())
-
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
